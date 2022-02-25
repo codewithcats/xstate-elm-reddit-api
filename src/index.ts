ce4dec57e211a2ad680dff559f929fa8cae6ab4a
Tanawat Tassana <amp.tanawat@gmail.com>
2022-02-25 16:13:44 +0700

    Create reddit machine

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import { createMachine, assign } from "xstate";
+
+type Context = { subreddit: string };
+type SelectEvent = { type: "SELECT"; name: string };
+type RedditEvents = SelectEvent;
+
+function invokeFetchSubreddit(context: Context) {
+  const { subreddit } = context;
+
+  return fetch(`https://www.reddit.com/r/${subreddit}.json`)
+    .then((response) => response.json())
+    .then((json) => json.data.children.map((child) => child.data));
+}
+
+const redditMachine = createMachine({
+  schema: {
+    context: {} as Context,
+    events: {} as RedditEvents,
+  },
+  id: "reddit",
+  context: {
+    subreddit: null,
+  },
+  states: {
+    idle: {},
+    selected: {},
+  },
+  initial: "idle",
+  on: {
+    SELECT: {
+      target: ".selected",
+      actions: assign<Context, SelectEvent>({
+        subreddit: (_, event) => event.name,
+      }),
+    },
+  },
+});
